a47cd0113b04e046e8bfa3176eb472a8d766a9ed
thangdo123 <thangdd3108@gmail.com>
2024-10-11 16:31:25 +0700

    feat/config columns desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'aot',
   content: ['./theme/**/*.liquid'],
   theme: {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    // Safelist classes from common patterns
+    { pattern: /.*/ }, // This wildcard pattern will match all utilities
+  ],
 };
