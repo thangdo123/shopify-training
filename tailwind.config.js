1d270cd7282ddfda436395610744af8e8a43067e
ThangDD-BSS <thangdd@bsscommerce.com>
2024-10-15 18:03:13 +0700

    feat/search-bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'aot',
   content: ['./theme/**/*.liquid'],
   theme: {
     extend: {},
   },
   plugins: [],
-  safelist: [
-    // Safelist classes from common patterns
-    { pattern: /.*/ }, // This wildcard pattern will match all utilities
-  ],
 };
